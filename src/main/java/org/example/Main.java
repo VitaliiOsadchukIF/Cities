6ad5b072156b086fe33394cbfef0c15d925520ba
VitaliiOsadchukIF <radistje@gmail.com>
2023-09-04 10:44:07 +0300

    add main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.example.database.GsonParser;
+import org.example.gameapp.ShowInitialWindow;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        GsonParser gsonParser = new GsonParser();
+        System.out.println(gsonParser.getCityNames());
+        //GameApp.start();//
+        SwingUtilities.invokeLater(ShowInitialWindow::show);
+
+    }
+}
