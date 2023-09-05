2a0c1f96aa9930eaf85e1b2f50614f6fe06ad0b5
Serg <moon.linexx@gmail.com>
2023-09-05 19:58:49 +0300

    last2 corrected

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,5 @@ public class Main {
         GsonParser gsonParser = new GsonParser();
         System.out.println(gsonParser.getCityNames());
         SwingUtilities.invokeLater(ShowInitialWindow::show);
-
     }
 }
