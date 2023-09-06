5a70f9f2c015dabf4fa71867b443787dfb704ff9
VitaliiOsadchukIF <radistje@gmail.com>
2023-09-06 14:13:34 +0300

    corrected all project

--- src/main/java/org/example/Winner.java
+++ src/main/java/org/example/Winner.java
@@ -3,16 +3,31 @@ package org.example;
 import javax.swing.*;
 
 public class Winner {
+    private static String notification = "Notification";
+
     public void userHasGivenUp(String textFromTextField) {
         if (textFromTextField.equals("I give up")) {
-            JOptionPane.showMessageDialog(null, "Every defeat is another step towards victory. \n This time the computer won", "Notification",
+            JOptionPane.showMessageDialog(null, "Every defeat is another step towards victory." +
+                            "\n This time the computer won", notification,
                     JOptionPane.INFORMATION_MESSAGE);
         }
     }
-    public void cityNotFound() {
-        JOptionPane.showMessageDialog(null,
-                "The game is over because the city is not found.",
-                "Notification", JOptionPane.INFORMATION_MESSAGE);
+    public void cityNotFound(String getComputerMove) {
+        if (getComputerMove.isEmpty()) {
+            JOptionPane.showMessageDialog(null,
+                    "The game is over because the city is not found.",
+                    notification, JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+        }
+    }
+
+    public void isPlayerWon(String getComputerMove) {
+        if (getComputerMove.isEmpty()) {
+            JOptionPane.showMessageDialog(null,
+                    "Player Won.",
+                    "Notification", JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+        }
     }
 }
 
--- src/main/java/org/example/gameapp/ShowInstructionsWindow.java
+++ src/main/java/org/example/gameapp/ShowInstructionsWindow.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class ShowInstructionsWindow {
     public static void show() {
         JFrame instructionsFrame = new JFrame("Instructions");
-        instructionsFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        instructionsFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         instructionsFrame.setSize(400, 200);
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -16,12 +16,12 @@ public class ShowInstructionsWindow {
 
         instructionsFrame.getContentPane().setBackground(Color.GRAY);
 
-        JTextArea instructionsArea = new JTextArea("1. You have to smile when playing this game )))\n" +
-                "2. Then write your favorite city\n" +
-                "3. The computer will write its city on the last letter\n" +
-                "4. Then you also enter the city that begins with \n the last letter of the entered city\n" +
-                "5. Cities should not be repeated");
-        instructionsArea.setEditable(false);
+        JLabel instructionsArea = new JLabel();
+        instructionsArea.setText("<html>1. You have to smile when playing this game )))" +
+                "<br>2. Then write your favorite city" +
+                "<br>3. The computer will write its city on the last letter" +
+                "<br>4. Then you also enter the city that begins with <br> the last letter of the entered city" +
+                "<br>5. Cities should not be repeated");
 
         Font boldFont = new Font("Arial", Font.BOLD, 14);
         instructionsArea.setFont(boldFont);
